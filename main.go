5a1af805c19a069bb7acccfc5046c704c100dc66
Rumi Allbert <rumiallbert@gmail.com>
2022-10-27 13:42:37 -0400

    First commit

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import ()
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
 
 func main() {
+	godotenv.Load(".env")
+	bot := slacker.NewClient(os.Getenv("STACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
+		Description: "Send any query to Wolfram",
+		Example:     "What is the weather in San Francisco?",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			query := request.Param("message")
+			client.Parse
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
